3809b0b9a1cb73bd2c3ee6708b276cdf6646a4af
Haider Ali <haiderkhalid602@gmail.com>
2023-05-13 01:11:55 +0500

    All Users View

---
+++ resources/views/addUser.blade.php
@@ -0,0 +1,1 @@
+<p>We're on the Add User Page</p>
---
+++ resources/views/crud.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel CRUD</title>
+</head>
+<body>
+    <form action='{{route("addUser")}}'>
+        <button>Add User</button>
+    </form>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+        </tr>
+    @foreach($users as $user)
+        <tr>
+            <td>{{$user->id}}</td>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->password}}</td>
+        </tr>
+    @endforeach
+    </table>
+
+
+
+</body>
+</html>
